d74223214240f4b1b95a412aad365d4e3de44839
shashankredemption <14sbhargava@gmail.com>
2013-10-06 02:55:20 -0400

    CLOsE OUT BOX BITCHES

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,25 @@
 // Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+a.boxclose{
+    float:right;
+    margin-top:-30px;
+    margin-right:-30px;
+    cursor:pointer;
+    color: #fff;
+    border: 1px solid #AEAEAE;
+    border-radius: 30px;
+    background: #605F61;
+    font-size: 31px;
+    font-weight: bold;
+    display: inline-block;
+    line-height: 0px;
+    padding: 11px 3px;       
+}
+
+.boxclose:before {
+    content: "×";
+}
 
 words = document.body.innerText.split(" ").length;
 
